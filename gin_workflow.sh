f079b37dddf3d619769394f6307d235b828c8763
Karine EM <karineven79@gmail.com>
2026-03-08 10:55:00 +0000

    Update gin_workflow.sh

--- gin_workflow.sh
+++ gin_workflow.sh
@@ -21,53 +21,84 @@ cd "${PROJECT_DIR}"
 # =====================
 # 1) Profile the project
 # =====================
-java -Dtinylog.level="${TINYLOG_LEVEL}" \
-  -cp "${GIN_JAR}" \
-  gin.util.Profiler \
-  -r 20 \
-  -mavenHome "${MAVEN_HOME}" \
-  -p "${PROJECT_NAME}" \
-  -d . \
-  -o "${PROJECT_NAME}.Profiler_output.csv"
+case $GINOPTION in
+    profile | PROFILE | prof | profiling)
+        java -Dtinylog.level="${TINYLOG_LEVEL}" \
+          -cp "${GIN_JAR}" \
+          gin.util.Profiler \
+          -r 20 \
+          -mavenHome "${MAVEN_HOME}" \
+          -p "${PROJECT_NAME}" \
+          -d . \
+          -o "${PROJECT_NAME}.Profiler_output.csv"
 
 # ==========================
 # 2) Masking Random Search
 # ==========================
-mkdir -p "${RESULTS_DIR}"
-
-java -Dtinylog.level="${TINYLOG_LEVEL}" \
-  -cp "${GIN_JAR}" \
-  gin.util.RandomSampler \
-  -j \
-  -p "${PROJECT_NAME}" \
-  -d . \
-  -m "${PROJECT_NAME}.Profiler_output.csv" \
-  -o "${RESULTS_DIR}/${PROJECT_NAME}.RandomSampler_1000_output.${MODEL}.csv" \
-  -mavenHome "${MAVEN_HOME}" \
-  -timeoutMS 10000 \
-  -et gin.edit.llm.LLMMaskedStatement \
-  -mt "${MODEL}" \
-  -pt MASKED \
-  -pn 1000 \
-  &> "${RESULTS_DIR}/${PROJECT_NAME}.RandomSampler_COMBINED_1000_stderrstdout.${MODEL}.txt"
+    random | Random | rand | RANDOM)
+        mkdir -p "${RESULTS_DIR}"
+        
+        java -Dtinylog.level="${TINYLOG_LEVEL}" \
+          -cp "${GIN_JAR}" \
+          gin.util.RandomSampler \
+          -j \
+          -p "${PROJECT_NAME}" \
+          -d . \
+          -m "${PROJECT_NAME}.Profiler_output.csv" \
+          -o "${RESULTS_DIR}/${PROJECT_NAME}.RandomSampler_1000_output.${MODEL}.csv" \
+          -mavenHome "${MAVEN_HOME}" \
+          -timeoutMS 10000 \
+          -et gin.edit.llm.LLMMaskedStatement \
+          -mt "${MODEL}" \
+          -pt MASKED \
+          -pn 1000 \
+          &> "${RESULTS_DIR}/${PROJECT_NAME}.RandomSampler_COMBINED_1000_stderrstdout.${MODEL}.txt"
 
 # =========================
 # 3) Masking Local Search
 # =========================
-mkdir -p "${RESULTS_DIR}"
+    local | Local | LOCAL)
+        mkdir -p "${RESULTS_DIR}"
+        
+        java -Dtinylog.level="${TINYLOG_LEVEL}" \
+          -cp "${GIN_JAR}" \
+          gin.util.LocalSearchRuntime \
+          -j \
+          -p "${PROJECT_NAME}" \
+          -d . \
+          -m "${PROJECT_NAME}.Profiler_output.csv" \
+          -o "${RESULTS_DIR}/${PROJECT_NAME}.LocalSearchRuntime_COMBINED_50_output.${MODEL}.csv" \
+          -mavenHome "${MAVEN_HOME}" \
+          -timeoutMS 10000 \
+          -et gin.edit.llm.LLMMaskedStatement \
+          -mt "${LLM}" \
+          -pt MASKED \
+          -in 100 \
+          &> "${RESULTS_DIR}/${PROJECT_NAME}.LocalSearchRuntime_LLM_MASKED_50_stderrstdout.${MODEL}.txt"
 
-java -Dtinylog.level="${TINYLOG_LEVEL}" \
-  -cp "${GIN_JAR}" \
-  gin.util.LocalSearchRuntime \
-  -j \
-  -p "${PROJECT_NAME}" \
-  -d . \
-  -m "${PROJECT_NAME}.Profiler_output.csv" \
-  -o "${RESULTS_DIR}/${PROJECT_NAME}.LocalSearchRuntime_COMBINED_50_output.${MODEL}.csv" \
-  -mavenHome "${MAVEN_HOME}" \
-  -timeoutMS 10000 \
-  -et gin.edit.llm.LLMMaskedStatement \
-  -mt "${LLM}" \
-  -pt MASKED \
-  -in 100 \
-  &> "${RESULTS_DIR}/${PROJECT_NAME}.LocalSearchRuntime_LLM_MASKED_50_stderrstdout.${MODEL}.txt"
+# =========================
+# 4) Local Search with PatchCat
+# =========================
+    LocalPatchCat | lpc | LPC |localpatchcat | LOCALPATCHCAT)
+        mkdir -p "${RESULTS_DIR}"
+        
+        java -Dtinylog.level="${TINYLOG_LEVEL}" \
+          -cp "${GIN_JAR}" \
+          gin.util.LocalSearchRuntime \
+          -j \
+          -p "${PROJECT_NAME}" \
+          -d . \
+          -m "${PROJECT_NAME}.Profiler_output.csv" \
+          -o "${RESULTS_DIR}/${PROJECT_NAME}.LocalSearchRuntime_COMBINED_50_output.${MODEL}.csv" \
+          -mavenHome "${MAVEN_HOME}" \
+          -timeoutMS 10000 \
+          -et gin.edit.llm.LLMMaskedStatement \
+          -mt "${LLM}" \
+          -pc \
+          -pt MASKED \
+          -in 100 \
+          &> "${RESULTS_DIR}/${PROJECT_NAME}.LocalSearchRuntime_LLM_MASKED_50_stderrstdout.${MODEL}.txt"  
+    *)
+        echo "Option is unknown"
+    ;;
+esac
